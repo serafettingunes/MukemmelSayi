8ba62be21072862b72a6aa82f33a891486359846
serafettingunes <54938342+attila40x@users.noreply.github.com>
2021-01-20 18:02:35 +0300

    Add project files.

---
+++ MukemmelSayi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MukemmelSayi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tpl = 0;
+            Console.Write("Bir sayı girin: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= x - 1; i++)
+            {
+                if (x % i == 0)
+                {
+                    tpl = tpl + i;
+                }
+            }
+            if (x == tpl)
+            {
+                Console.WriteLine("Girdiğiniz sayı mükemmel sayıdır");
+            }
+            else
+            {
+                Console.WriteLine("Girdiğiniz sayı mükemmel sayı değildir");
+            }
+        }
+    }
+}
